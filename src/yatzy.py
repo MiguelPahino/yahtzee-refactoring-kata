088dbbd8cedcbe3a663c0c6218ce002d1023123a
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-01-26 12:44:44 +0100

    refactor(yatzy): Refactorizar la funcion three_of_a_kind.
    Co-authored-by: Miguel Gutiérrez Pahino <MiguelPahino@users.noreply.github.com>

--- src/yatzy.py
+++ src/yatzy.py
@@ -72,17 +72,12 @@ class Yatzy:
         )
 
     @staticmethod
-    def three_of_a_kind(d1, d2, d3, d4, d5):
-        t = [0] * 6
-        t[d1 - 1] += 1
-        t[d2 - 1] += 1
-        t[d3 - 1] += 1
-        t[d4 - 1] += 1
-        t[d5 - 1] += 1
-        for i in range(6):
-            if t[i] >= 3:
-                return (i + 1) * 3
-        return 0
+    def three_of_a_kind(*dice):
+        THREE = Pips.THREE.value
+        return next(
+            (number * THREE for number in Pips.values() if dice.count(number) >= THREE),
+            0,
+        )
 
     @staticmethod
     def smallStraight(d1, d2, d3, d4, d5):
